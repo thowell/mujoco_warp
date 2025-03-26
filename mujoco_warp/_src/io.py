53a2f91474d8abfc74309a99039478f7cbf51fc2
Taylor Howell <taylorhowell@google.com>
2025-03-26 21:19:39 +0000

    add eq_type check to loop

--- mujoco_warp/_src/io.py
+++ mujoco_warp/_src/io.py
@@ -30,19 +30,11 @@ def put_model(mjm: mujoco.MjModel) -> types.Model:
     (mjm.actuator_dyntype, types.DynType, "Actuator dynamics type"),
     (mjm.actuator_gaintype, types.GainType, "Gain type"),
     (mjm.actuator_biastype, types.BiasType, "Bias type"),
+    (mjm.eq_type, types.EqType, "Equality constraint types"),
   ):
     unsupported = ~np.isin(field, list(field_types))
     if unsupported.any():
       raise NotImplementedError(f"{field_str} {field[unsupported]} not supported.")
-
-  eq_type_unsupported = ~np.isin(mjm.eq_type, list(types.EqType))
-  if eq_type_unsupported.any():
-    raise NotImplementedError(
-      f"Equality constraint types {mjm.eq_type[eq_type_unsupported]} are unsupported."
-    )
-
-  if mjm.neq > 0:
-    raise NotImplementedError("Equality constraints are unsupported.")
 
   if mjm.nsensor > 0:
     raise NotImplementedError("Sensors are unsupported.")
