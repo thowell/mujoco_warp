d98a9f7879dd6936a37997dcdfedb0e89d028741
nvtw <110816143+nvtw@users.noreply.github.com>
2025-04-09 15:31:11 +0200

    Implement plane vs cylinder collision (#104)
    * Implement plane vs cylinder collision
    
    * Make implementation more similar to mujoco C
    
    * Improve the tests

--- mujoco_warp/_src/collision_primitive.py
+++ mujoco_warp/_src/collision_primitive.py
@@ -264,6 +264,83 @@ def plane_box(
       break
 
 
+@wp.func
+def plane_cylinder(
+  plane: Geom,
+  cylinder: Geom,
+  worldid: int,
+  d: Data,
+  margin: float,
+  geom_indices: wp.vec2i,
+):
+  """Calculates contacts between a cylinder and a plane."""
+  # Extract plane normal and cylinder axis
+  n = plane.normal
+  axis = wp.vec3(cylinder.rot[0, 2], cylinder.rot[1, 2], cylinder.rot[2, 2])
+
+  # Project, make sure axis points toward plane
+  prjaxis = wp.dot(n, axis)
+  if prjaxis > 0:
+    axis = -axis
+    prjaxis = -prjaxis
+
+  # Compute normal distance from plane to cylinder center
+  dist0 = wp.dot(cylinder.pos - plane.pos, n)
+
+  # Remove component of -normal along cylinder axis
+  vec = axis * prjaxis - n
+  len_sqr = wp.dot(vec, vec)
+
+  # If vector is nondegenerate, normalize and scale by radius
+  # Otherwise use cylinder's x-axis scaled by radius
+  vec = wp.where(
+    len_sqr >= 1e-12,
+    vec * (cylinder.size[0] / wp.sqrt(len_sqr)),
+    wp.vec3(cylinder.rot[0, 0], cylinder.rot[1, 0], cylinder.rot[2, 0])
+    * cylinder.size[0],
+  )
+
+  # Project scaled vector on normal
+  prjvec = wp.dot(vec, n)
+
+  # Scale cylinder axis by half-length
+  axis = axis * cylinder.size[1]
+  prjaxis = prjaxis * cylinder.size[1]
+
+  frame = make_frame(n)
+
+  # First contact point (end cap closer to plane)
+  dist1 = dist0 + prjaxis + prjvec
+  if dist1 <= margin:
+    pos1 = cylinder.pos + vec + axis - n * (dist1 * 0.5)
+    write_contact(d, dist1, pos1, frame, margin, geom_indices, worldid)
+  else:
+    # If nearest point is above margin, no contacts
+    return
+
+  # Second contact point (end cap farther from plane)
+  dist2 = dist0 - prjaxis + prjvec
+  if dist2 <= margin:
+    pos2 = cylinder.pos + vec - axis - n * (dist2 * 0.5)
+    write_contact(d, dist2, pos2, frame, margin, geom_indices, worldid)
+
+  # Try triangle contact points on side closer to plane
+  prjvec1 = -prjvec * 0.5
+  dist3 = dist0 + prjaxis + prjvec1
+  if dist3 <= margin:
+    # Compute sideways vector scaled by radius*sqrt(3)/2
+    vec1 = wp.cross(vec, axis)
+    vec1 = wp.normalize(vec1) * (cylinder.size[0] * wp.sqrt(3.0) * 0.5)
+
+    # Add contact point A - adjust to closest side
+    pos3 = cylinder.pos + vec1 + axis - vec * 0.5 - n * (dist3 * 0.5)
+    write_contact(d, dist3, pos3, frame, margin, geom_indices, worldid)
+
+    # Add contact point B - adjust to closest side
+    pos4 = cylinder.pos - vec1 + axis - vec * 0.5 - n * (dist3 * 0.5)
+    write_contact(d, dist3, pos4, frame, margin, geom_indices, worldid)
+
+
 @wp.kernel
 def _primitive_narrowphase(
   m: Model,
@@ -300,6 +377,8 @@ def _primitive_narrowphase(
     capsule_capsule(geom1, geom2, worldid, d, margin, geoms)
   elif type1 == int(GeomType.SPHERE.value) and type2 == int(GeomType.CAPSULE.value):
     sphere_capsule(geom1, geom2, worldid, d, margin, geoms)
+  elif type1 == int(GeomType.PLANE.value) and type2 == int(GeomType.CYLINDER.value):
+    plane_cylinder(geom1, geom2, worldid, d, margin, geoms)
 
 
 def primitive_narrowphase(m: Model, d: Data):
