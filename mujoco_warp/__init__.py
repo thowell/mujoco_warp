d7e021a8b10561babb7bd4451ba5087462784f3e
Taylor Howell <taylorhowell@google.com>
2025-04-09 16:20:47 +0100

    __init__.py

--- mujoco_warp/__init__.py
+++ mujoco_warp/__init__.py
@@ -36,8 +36,8 @@ from ._src.passive import passive as passive
 from ._src.sensor import sensor_acc as sensor_acc
 from ._src.sensor import sensor_pos as sensor_pos
 from ._src.sensor import sensor_vel as sensor_vel
-from ._src.smooth import com_pos as com_pos
 from ._src.smooth import camlight as camlight
+from ._src.smooth import com_pos as com_pos
 from ._src.smooth import com_vel as com_vel
 from ._src.smooth import crb as crb
 from ._src.smooth import factor_m as factor_m
