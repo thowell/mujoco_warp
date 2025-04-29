fc6224f980b6275c104a9c3ad8f6b80ee00ccf06
Kenny Vilella <kvilella@nvidia.com>
2025-04-29 10:58:27 +0800

    Added WIP file to run benchmarks with various env and robots

---
+++ mujoco_warp/test_data/kitchen_env/kitchen_benchmark.py
@@ -0,0 +1,241 @@
+# Copyright 2025 The Newton Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Run kitchen benchmarks with various robots."""
+
+import inspect
+from typing import Sequence
+
+import mujoco
+from dm_control import mjcf
+import numpy as np
+import warp as wp
+from absl import app
+from absl import flags
+from etils import epath
+import os
+
+import mujoco_warp as mjwarp
+
+_SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+_FUNCTION = flags.DEFINE_enum(
+  "function",
+  "step",
+  [n for n, _ in inspect.getmembers(mjwarp, inspect.isfunction)],
+  "the function to run",
+)
+_ROBOT = flags.DEFINE_enum(
+  "robot", "franka", ["franka"], "the robot to use",
+)
+_NSTEP = flags.DEFINE_integer("nstep", 1000, "number of steps per rollout")
+_BATCH_SIZE = flags.DEFINE_integer("batch_size", 8192, "number of parallel rollouts")
+_SOLVER = flags.DEFINE_enum(
+  "solver", None, ["cg", "newton"], "Override model constraint solver"
+)
+_ITERATIONS = flags.DEFINE_integer(
+  "iterations", None, "Override model solver iterations"
+)
+_LS_ITERATIONS = flags.DEFINE_integer(
+  "ls_iterations", None, "Override model linesearch iterations"
+)
+_LS_PARALLEL = flags.DEFINE_bool("ls_parallel", False, "solve with parallel linesearch")
+_IS_SPARSE = flags.DEFINE_bool("is_sparse", None, "Override model sparse config")
+_CONE = flags.DEFINE_enum(
+  "cone", "pyramidal", ["pyramidal", "elliptic"], "Friction cone type"
+)
+_NCONMAX = flags.DEFINE_integer(
+  "nconmax",
+  None,
+  "Override default maximum number of contacts in a batch physics step.",
+)
+_NJMAX = flags.DEFINE_integer(
+  "njmax",
+  None,
+  "Override default maximum number of constraints in a batch physics step.",
+)
+_OUTPUT = flags.DEFINE_enum(
+  "output", "text", ["text", "tsv"], "format to print results"
+)
+_CLEAR_KERNEL_CACHE = flags.DEFINE_bool(
+  "clear_kernel_cache", False, "Clear kernel cache (to calculate full JIT time)"
+)
+_EVENT_TRACE = flags.DEFINE_bool("event_trace", False, "Provide a full event trace")
+_MEASURE_ALLOC = flags.DEFINE_bool(
+  "measure_alloc", False, "Measure how much of nconmax, njmax is used."
+)
+
+def _main(argv: Sequence[str]):
+  """Runs testpeed function."""
+  wp.init()
+
+  ## Verifying that the two xml models exist
+  kitchen_path = epath.Path("kitchen_env.xml")
+  robot_path = epath.Path("../humanoid/humanoid.xml")
+  if not kitchen_path.exists():
+    raise FileNotFoundError(f"kitchen_env.xml not found.")
+  if not robot_path.exists():
+    raise FileNotFoundError(f"robot xml not found.")
+
+  ## Loading the two models and creating a xml string
+  # Loading the robot first to not have issue with freeejoint
+  mjcf_model = mjcf.from_path(robot_path)
+  env_model = mjcf.from_path(kitchen_path)
+  mjcf_model.attach(env_model)
+  xml_string = mjcf_model.to_xml_string()
+
+  ## Post-processing to add proper mesh path
+  original_model = mujoco.MjSpec.from_file(kitchen_path.as_posix())
+  # For each mesh in the original model, search the file attribute and replace it by the
+  # path from the original model.
+  for i in range(len(original_model.meshes)):
+      updated_path = _SCRIPT_DIR + "/assets/" + original_model.meshes[i].file
+      start_mesh_string = xml_string.find(original_model.meshes[i].name)
+      start_file_string = xml_string.find("file", start_mesh_string) + 6
+      end_file_string = xml_string.find('"', start_file_string)
+      xml_string = xml_string[:start_file_string] + updated_path + xml_string[end_file_string:]
+
+  ## Post-processing to remove keyframes
+  start_keyframe_string = xml_string.find("<keyframe>")
+  end_keyframe_string = xml_string.find("</keyframe>", start_keyframe_string) + 11
+  xml_string = xml_string[:start_keyframe_string] + xml_string[end_keyframe_string:]
+
+  ## Creating the mujoco model and adjusting its properties
+  mjm = mujoco.MjModel.from_xml_string(xml_string)
+  if _CONE.value == "pyramidal":
+    mjm.opt.cone = mujoco.mjtCone.mjCONE_PYRAMIDAL
+  elif _CONE.value == "elliptic":
+    mjm.opt.cone = mujoco.mjtCone.mjCONE_ELLIPTIC
+
+  if _IS_SPARSE.value == True:
+    mjm.opt.jacobian = mujoco.mjtJacobian.mjJAC_SPARSE
+  elif _IS_SPARSE.value == False:
+    mjm.opt.jacobian = mujoco.mjtJacobian.mjJAC_DENSE
+
+  if _SOLVER.value == "cg":
+    mjm.opt.solver = mujoco.mjtSolver.mjSOL_CG
+  elif _SOLVER.value == "newton":
+    mjm.opt.solver = mujoco.mjtSolver.mjSOL_NEWTON
+
+  if _ITERATIONS.value is not None:
+    mjm.opt.iterations = _ITERATIONS.value
+
+  if _LS_ITERATIONS.value is not None:
+    mjm.opt.ls_iterations = _LS_ITERATIONS.value
+
+  ## Creating the mujoco data and populating some constraints
+  mjd = mujoco.MjData(mjm)
+  mujoco.mj_forward(mjm, mjd)
+
+  m = mjwarp.put_model(mjm)
+  m.opt.ls_parallel = _LS_PARALLEL.value
+  d = mjwarp.put_data(
+    mjm, mjd, nworld=_BATCH_SIZE.value, nconmax=_NCONMAX.value, njmax=_NJMAX.value
+  )
+
+  if _CLEAR_KERNEL_CACHE.value:
+    wp.clear_kernel_cache()
+
+  solver_name = {1: "CG", 2: "Newton"}[mjm.opt.solver]
+  linesearch_name = {True: "parallel", False: "iterative"}[m.opt.ls_parallel]
+  print(
+    f"Model nbody: {m.nbody} nv: {m.nv} ngeom: {m.ngeom} "
+    f"is_sparse: {_IS_SPARSE.value} solver: {solver_name} "
+    f"iterations: {m.opt.iterations} ls_iterations: {m.opt.ls_iterations} "
+    f"linesearch: {linesearch_name}"
+  )
+  print(f"Data nworld: {d.nworld} nconmax: {d.nconmax} njmax: {d.njmax}")
+  print(f"Rolling out {_NSTEP.value} steps at dt = {m.opt.timestep:.3f}...")
+  jit_time, run_time, trace, ncon, nefc = mjwarp.benchmark(
+    mjwarp.__dict__[_FUNCTION.value],
+    m,
+    d,
+    _NSTEP.value,
+    _EVENT_TRACE.value,
+    _MEASURE_ALLOC.value,
+  )
+  steps = _BATCH_SIZE.value * _NSTEP.value
+
+  name = argv[0]
+  if _OUTPUT.value == "text":
+    print(f"""
+Summary for {_BATCH_SIZE.value} parallel rollouts
+
+ Total JIT time: {jit_time:.2f} s
+ Total simulation time: {run_time:.2f} s
+ Total steps per second: {steps / run_time:,.0f}
+ Total realtime factor: {steps * m.opt.timestep / run_time:,.2f} x
+ Total time per step: {1e9 * run_time / steps:.2f} ns""")
+    if trace:
+      print("\nEvent trace:\n")
+
+      def _print_trace(trace, indent):
+        for k, v in trace.items():
+          times, sub_trace = v
+          if len(times) == 1:
+            print("  " * indent + f"{k}: {1e6 * times[0] / steps:.2f}")
+          else:
+            print("  " * indent + f"{k}: [ ", end="")
+            for i in range(len(times)):
+              print(f"{1e6 * times[i] / steps:.2f}", end="")
+              print(", " if i < len(times) - 1 else " ", end="")
+            print("]")
+          _print_trace(sub_trace, indent + 1)
+
+      _print_trace(trace, 0)
+    if ncon and nefc:
+      num_buckets = 10
+      idx = 0
+      ncon_matrix, nefc_matrix = [], []
+      for i in range(num_buckets):
+        size = _NSTEP.value // num_buckets + (i < (_NSTEP.value % num_buckets))
+        ncon_arr = np.array(ncon[idx : idx + size])
+        nefc_arr = np.array(nefc[idx : idx + size])
+        ncon_matrix.append(
+          [np.mean(ncon_arr), np.std(ncon_arr), np.min(ncon_arr), np.max(ncon_arr)]
+        )
+        nefc_matrix.append(
+          [np.mean(nefc_arr), np.std(nefc_arr), np.min(nefc_arr), np.max(nefc_arr)]
+        )
+        idx += size
+
+      def _print_table(matrix, headers):
+        num_cols = len(headers)
+        col_widths = [
+          max(len(f"{row[i]:g}") for row in matrix) for i in range(num_cols)
+        ]
+        col_widths = [max(col_widths[i], len(headers[i])) for i in range(num_cols)]
+
+        print("  ".join(f"{headers[i]:<{col_widths[i]}}" for i in range(num_cols)))
+        print("-" * sum(col_widths) + "--" * 3)  # Separator line
+        for row in matrix:
+          print("  ".join(f"{row[i]:{col_widths[i]}g}" for i in range(num_cols)))
+
+      print("\nncon alloc:\n")
+      _print_table(ncon_matrix, ("mean", "std", "min", "max"))
+      print("\nnefc alloc:\n")
+      _print_table(nefc_matrix, ("mean", "std", "min", "max"))
+
+  elif _OUTPUT.value == "tsv":
+    name = name.split("/")[-1].replace("testspeed_", "")
+    print(f"{name}\tjit: {jit_time:.2f}s\tsteps/second: {steps / run_time:.0f}")
+
+
+def main():
+  app.run(_main)
+
+
+if __name__ == "__main__":
+  main()
