72dec8ef56f7a31e5a2e7e7890505012209bf8f8
Alessio Quaglino <quaglino@google.com>
2025-05-19 14:38:23 +0100

    fix indexing

--- mujoco_warp/_src/passive.py
+++ mujoco_warp/_src/passive.py
@@ -442,10 +442,10 @@ def _flex_bending(
   f = 0  # TODO(quaglino): this should become a function of t
 
   v = wp.vec4(
-    flex_edge[edgeid + flex_edgeadr[f], 0],
-    flex_edge[edgeid + flex_edgeadr[f], 1],
-    flex_edgeflap[edgeid + flex_edgeadr[f], 0],
-    flex_edgeflap[edgeid + flex_edgeadr[f], 1],
+    flex_edge[edgeid + flex_edgeadr[f]][0],
+    flex_edge[edgeid + flex_edgeadr[f]][1],
+    flex_edgeflap[edgeid + flex_edgeadr[f]][0],
+    flex_edgeflap[edgeid + flex_edgeadr[f]][1],
   )
 
   if v[3] == -1:
