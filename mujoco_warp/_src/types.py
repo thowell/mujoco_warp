8defdaea8465115cd4b8f744a456a52093653b62
Taylor Howell <taylorhowell@google.com>
2025-05-30 10:47:30 +0100

    add todo

--- mujoco_warp/_src/types.py
+++ mujoco_warp/_src/types.py
@@ -26,6 +26,7 @@ MJ_NREF = mujoco.mjNREF
 MJ_NIMP = mujoco.mjNIMP
 
 
+# TODO(team): add check that all wp.launch_tiled 'block_dim' settings are configurable
 class BlockDim:
   """
   Block dimension 'block_dim' settings for wp.launch_tiled.
