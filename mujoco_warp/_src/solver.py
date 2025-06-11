883a768e7d7c84aac2abcd457a06046f769847f0
adenzler-nvidia <116633880+adenzler-nvidia@users.noreply.github.com>
2025-06-11 11:15:56 +0200

    Skip solver if there are no constraints (#337)
    * add conditional node for solver constraints
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * fix usage
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * add skip criteria for no pairs
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * remove opt change
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    ---------
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- mujoco_warp/_src/solver.py
+++ mujoco_warp/_src/solver.py
@@ -2653,6 +2653,13 @@ def create_context(m: types.Model, d: types.Data, grad: bool = True):
 
 @event_scope
 def solve(m: types.Model, d: types.Data):
+  if m.opt.graph_conditional:
+    wp.capture_if(condition=d.nefc, on_true=_solve, on_false=None, m=m, d=d)
+  else:
+    _solve(m, d)
+
+
+def _solve(m: types.Model, d: types.Data):
   """Finds forces that satisfy constraints."""
   # warmstart
   wp.copy(d.qacc, d.qacc_warmstart)
