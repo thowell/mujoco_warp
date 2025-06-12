7e27a942d9182435d5d4b6c601260d1952d9f57d
adenzler-nvidia <116633880+adenzler-nvidia@users.noreply.github.com>
2025-06-13 01:09:48 +0200

    Optimization: fuse linesearch_init_quad_gauss (#357)
    * make the zeroing disapear
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * commit WIP
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * formatting
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * revert testspeed changes
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * revert viewer changes
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * revert conftest changes
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * formatting
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * add synchronize back to viewer
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    ---------
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- mujoco_warp/_src/solver.py
+++ mujoco_warp/_src/solver.py
@@ -1107,21 +1107,6 @@ def linesearch_jv_fused(nv: int, dofs_per_thread: int):
 
 
 @wp.kernel
-def linesearch_zero_quad_gauss(
-  # Data in:
-  efc_done_in: wp.array(dtype=bool),
-  # Data out:
-  efc_quad_gauss_out: wp.array(dtype=wp.vec3),
-):
-  worldid = wp.tid()
-
-  if efc_done_in[worldid]:
-    return
-
-  efc_quad_gauss_out[worldid] = wp.vec3(0.0)
-
-
-@wp.kernel
 def linesearch_init_quad_gauss(
   # Model:
   nv: int,
@@ -1135,16 +1120,19 @@ def linesearch_init_quad_gauss(
   # Data out:
   efc_quad_gauss_out: wp.array(dtype=wp.vec3),
 ):
-  worldid, dofid = wp.tid()
+  worldid = wp.tid()
   if efc_done_in[worldid]:
     return
 
-  search = efc_search_in[worldid, dofid]
-  quad_gauss = wp.vec3()
-  quad_gauss[0] = efc_gauss_in[worldid] / float(nv)
-  quad_gauss[1] = search * (efc_Ma_in[worldid, dofid] - qfrc_smooth_in[worldid, dofid])
-  quad_gauss[2] = 0.5 * search * efc_mv_in[worldid, dofid]
-  wp.atomic_add(efc_quad_gauss_out, worldid, quad_gauss)
+  quad_gauss_0 = efc_gauss_in[worldid]
+  quad_gauss_1 = float(0.0)
+  quad_gauss_2 = float(0.0)
+  for i in range(nv):
+    search = efc_search_in[worldid, i]
+    quad_gauss_1 += search * (efc_Ma_in[worldid, i] - qfrc_smooth_in[worldid, i])
+    quad_gauss_2 += 0.5 * search * efc_mv_in[worldid, i]
+
+  efc_quad_gauss_out[worldid] = wp.vec3(quad_gauss_0, quad_gauss_1, quad_gauss_2)
 
 
 @wp.kernel
@@ -1313,17 +1301,9 @@ def _linesearch(m: types.Model, d: types.Data):
 
   # prepare quadratics
   # quad_gauss = [gauss, search.T @ Ma - search.T @ qfrc_smooth, 0.5 * search.T @ mv]
-  # TOOD(team): is zero_() better here?
-  wp.launch(
-    linesearch_zero_quad_gauss,
-    dim=(d.nworld),
-    inputs=[d.efc.done],
-    outputs=[d.efc.quad_gauss],
-  )
-
   wp.launch(
     linesearch_init_quad_gauss,
-    dim=(d.nworld, m.nv),
+    dim=(d.nworld),
     inputs=[m.nv, d.qfrc_smooth, d.efc.Ma, d.efc.search, d.efc.gauss, d.efc.mv, d.efc.done],
     outputs=[d.efc.quad_gauss],
   )
