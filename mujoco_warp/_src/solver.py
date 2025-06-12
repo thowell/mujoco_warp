e6db814d732e23b7a12cf8fea5244a73e68fd9bc
adenzler-nvidia <116633880+adenzler-nvidia@users.noreply.github.com>
2025-06-12 09:19:59 +0200

    Optimization: loop over nv in solve_init_jaref (#356)
    * first version
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * fixes, remove scaling
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * nv static
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * remove duplicated kernel
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * readd nv param
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    * linter
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>
    
    ---------
    
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- mujoco_warp/_src/solver.py
+++ mujoco_warp/_src/solver.py
@@ -1414,14 +1414,17 @@ def solve_init_jaref(
   # Data out:
   efc_Jaref_out: wp.array(dtype=float),
 ):
-  efcid, dofid = wp.tid()
+  efcid = wp.tid()
 
   if efcid >= nefc_in[0]:
     return
 
   worldid = efc_worldid_in[efcid]
-  jaref = efc_J_in[efcid, dofid] * qacc_in[worldid, dofid] - efc_aref_in[efcid] / float(nv)
-  wp.atomic_add(efc_Jaref_out, efcid, jaref)
+  jaref = float(0.0)
+  for i in range(nv):
+    jaref += efc_J_in[efcid, i] * qacc_in[worldid, i]
+
+  efc_Jaref_out[efcid] = jaref - efc_aref_in[efcid]
 
 
 @wp.kernel
@@ -2634,10 +2637,9 @@ def create_context(m: types.Model, d: types.Data, grad: bool = True):
   )
 
   # jaref = d.efc_J @ d.qacc - d.efc_aref
-  d.efc.Jaref.zero_()
   wp.launch(
     solve_init_jaref,
-    dim=(d.njmax, m.nv),
+    dim=(d.njmax),
     inputs=[m.nv, d.nefc, d.qacc, d.efc.worldid, d.efc.J, d.efc.aref],
     outputs=[d.efc.Jaref],
   )
