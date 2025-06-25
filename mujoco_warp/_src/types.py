079a7464e63d8a6a80eb803fc131c645d46d7340
Taylor Howell <taylorhowell@google.com>
2025-06-25 01:22:58 -0700

    remove unused MJ_NREF and MJ_NIMP (#391)

--- mujoco_warp/_src/types.py
+++ mujoco_warp/_src/types.py
@@ -23,8 +23,6 @@ MJ_MAXVAL = mujoco.mjMAXVAL
 MJ_MINIMP = mujoco.mjMINIMP  # minimum constraint impedance
 MJ_MAXIMP = mujoco.mjMAXIMP  # maximum constraint impedance
 MJ_MAXCONPAIR = mujoco.mjMAXCONPAIR
-MJ_NREF = mujoco.mjNREF
-MJ_NIMP = mujoco.mjNIMP
 
 
 # TODO(team): add check that all wp.launch_tiled 'block_dim' settings are configurable
