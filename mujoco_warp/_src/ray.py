2a4048732147b5dead7419d52dce16c7130c12ca
adenzler-nvidia <116633880+adenzler-nvidia@users.noreply.github.com>
2025-06-30 17:15:51 +0200

    move to lower-case tuple for array return type (#428)
    Signed-off-by: Alain Denzler <adenzler@nvidia.com>

--- mujoco_warp/_src/ray.py
+++ mujoco_warp/_src/ray.py
@@ -829,7 +829,7 @@ def ray(
   geomgroup: vec6 = None,
   flg_static: bool = True,
   bodyexclude: int = -1,
-) -> Tuple[wp.array2d(dtype=float), wp.array2d(dtype=int)]:
+) -> tuple[wp.array2d(dtype=float), wp.array2d(dtype=int)]:
   """Returns the distance at which rays intersect with primitive geoms.
 
   Args:
