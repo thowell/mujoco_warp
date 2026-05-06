cfb810af27b3869eaefab776afb0a8fbf54bc378
Taylor Howell <taylorhowell@google.com>
2026-05-06 22:19:48 +0100

    version guard

--- mujoco_warp/_src/forward_test.py
+++ mujoco_warp/_src/forward_test.py
@@ -29,6 +29,7 @@ from mujoco_warp import EnableBit
 from mujoco_warp import GainType
 from mujoco_warp import IntegratorType
 from mujoco_warp import test_data
+from mujoco_warp._src.util_pkg import check_version
 
 # tolerance for difference between MuJoCo and mjwarp smooth calculations - mostly
 # due to float precision
@@ -437,7 +438,10 @@ class ForwardTest(parameterized.TestCase):
         d_arr = d_arr.reshape(-1)
       elif arr == "qM":
         mjd_arr = np.zeros((mjm.nv, mjm.nv))
-        mujoco.mju_sym2dense(mjd_arr, mjd.M, mjm.M_rownnz, mjm.M_rowadr, mjm.M_colind)
+        if check_version("mujoco>=3.8.1.dev910242375"):
+          mujoco.mju_sym2dense(mjd_arr, mjd.M, mjm.M_rownnz, mjm.M_rowadr, mjm.M_colind)
+        else:
+          mujoco.mj_fullM(mjm, mjd_arr, mjd.qM)
         d_arr = d_arr[: mjm.nv, : mjm.nv]
       elif arr == "actuator_moment":
         actuator_moment = np.zeros((mjm.nu, mjm.nv))
