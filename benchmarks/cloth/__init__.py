e26888572ff1e298de8caa3b155b1fd214571143
Taylor Howell <taylorhowell@google.com>
2026-05-12 15:11:47 +0100

    benchmark render flex

--- benchmarks/cloth/__init__.py
+++ benchmarks/cloth/__init__.py
@@ -5,5 +5,16 @@ BENCHMARKS = [
     "nworld": 32,
     "nconmax": 4000,
     "njmax": 32000,
-  }
+  },
+  {
+    "name": "cloth_render",
+    "mjcf": "scene.xml",
+    "function": "render",
+    "nworld": 32,
+    "nconmax": 4000,
+    "njmax": 32000,
+    "nstep": 200,
+    "render_width": 64,
+    "render_height": 64,
+  },
 ]
