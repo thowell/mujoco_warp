74478eee1b5ade5ba85b903566502a16a2267811
Taylor Howell <taylorhowell@google.com>
2026-07-28 23:33:46 +0100

    mujoco v3.11.0 (#1563)

--- mujoco_warp/_src/derivative_test.py
+++ mujoco_warp/_src/derivative_test.py
@@ -25,7 +25,6 @@ import mujoco_warp as mjw
 from mujoco_warp import test_data
 from mujoco_warp._src import derivative
 from mujoco_warp._src import forward
-from mujoco_warp._src import util_pkg
 
 # tolerance for difference between MuJoCo and mjwarp smooth calculations - mostly
 # due to float precision
@@ -1240,11 +1239,8 @@ class DerivativeTest(parameterized.TestCase):
       """,
   }
 
-  # TODO(thowell): update implementation for free bodies
-  @absltest.skipIf(
-    util_pkg.check_version("mujoco>=3.10.1.dev948899583"),
-    "Requires MuJoCo < 3.10.1.dev948899583",
-  )
+  # TODO: update implementation for free bodies (https://github.com/google-deepmind/mujoco_warp/pull/1548)
+  @absltest.skip("Requires updating fluid derivative implementation for free bodies to match MuJoCo 3.11")
   @parameterized.product(
     scenario=list(_FLUID_SCENARIOS.keys()),
     jacobian=[mujoco.mjtJacobian.mjJAC_DENSE, mujoco.mjtJacobian.mjJAC_SPARSE],
--- mujoco_warp/_src/types_test.py
+++ mujoco_warp/_src/types_test.py
@@ -23,7 +23,6 @@ import warp as wp
 from absl.testing import absltest
 from absl.testing import parameterized
 
-from mujoco_warp._src import util_pkg
 from mujoco_warp._src.types import Data
 from mujoco_warp._src.types import Model
 from mujoco_warp._src.types import Option
@@ -41,11 +40,6 @@ class TypesTest(parameterized.TestCase):
     self.assertEqual(hash(tile_a), hash(tile_b))
     self.assertNotEqual(tile_a, tile_c)
 
-  # TODO(thowell): update Model field ordering
-  @absltest.skipIf(
-    util_pkg.check_version("mujoco>=3.10.1.dev948361631"),
-    "Requires MuJoCo < 3.10.1.dev948361631",
-  )
   @parameterized.parameters((mujoco.MjOption, Option), (mujoco.MjModel, Model), (mujoco.MjData, Data))
   def test_field_order(self, mj_class, mjw_class):
     """Tests that MJW field order matches MuJoCo, and all warp-only fields are at the end."""
