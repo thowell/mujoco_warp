b32c082e9a47300609b441d0cdeff67ecf0a5b4e
Taylor Howell <taylorhowell@google.com>
2026-09-09 16:26:40 +0100

    cylinder multiccd (#1653)
    * cylinder multiccd
    
    * address pr comments from kbayes

--- mujoco_warp/_src/io_test.py
+++ mujoco_warp/_src/io_test.py
@@ -2667,9 +2667,6 @@ class IOTest(parameterized.TestCase):
 
   # TODO(team): remove after implementing multicontact support for CCD pairs.
   @parameterized.parameters(
-    ("cylinder", "box"),
-    ("cylinder", "cylinder"),
-    ("cylinder", "mesh"),
     ("capsule", "cylinder"),
     ("capsule", "mesh"),
   )
@@ -2710,6 +2707,49 @@ class IOTest(parameterized.TestCase):
       mjwarp.put_model(mjm)
 
     mjm.opt.disableflags |= mujoco.mjtDisableBit.mjDSBL_MULTICCD
+    with warnings.catch_warnings():
+      warnings.simplefilter("error")
+      mjwarp.put_model(mjm)
+
+  @parameterized.parameters(
+    ("cylinder", "box"),
+    ("cylinder", "cylinder"),
+    ("cylinder", "mesh"),
+  )
+  def test_supported_cylinder_multiccd_no_warning(self, geom1_type, geom2_type):
+    """Tests that supported cylinder MultiCCD pairs emit no warning."""
+
+    def _make_geom_xml(gtype: str) -> str:
+      if gtype == "mesh":
+        return '<geom type="mesh" mesh="m"/>'
+      elif gtype in ("cylinder", "capsule"):
+        return f'<geom type="{gtype}" size=".1 .1"/>'
+      elif gtype == "sphere":
+        return '<geom type="sphere" size=".1"/>'
+      else:
+        return f'<geom type="{gtype}" size=".1 .1 .1"/>'
+
+    mesh_asset = '<mesh name="m" vertex="0 0 0 1 0 0 0 1 0 0 0 1"/>' if "mesh" in (geom1_type, geom2_type) else ""
+    mjm = mujoco.MjModel.from_xml_string(
+      f"""
+      <mujoco>
+        <asset>
+          {mesh_asset}
+        </asset>
+        <worldbody>
+          <body>
+            <freejoint/>
+            {_make_geom_xml(geom1_type)}
+          </body>
+          <body pos="0 0 .5">
+            <freejoint/>
+            {_make_geom_xml(geom2_type)}
+          </body>
+        </worldbody>
+      </mujoco>
+      """
+    )
+
     with warnings.catch_warnings():
       warnings.simplefilter("error")
       mjwarp.put_model(mjm)
